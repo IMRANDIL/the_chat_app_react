891d183301ad3706ea73d34fa82a544a0a661414
IMRANDIL <aliimranadil2@gmail.com>
2022-02-18 21:16:25 +0530

    yh

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -27,7 +27,7 @@ const ChatFeed = (props) => {
                 <div key={`msg_${index}`} style={{ width: '100%' }}>
                     <div className="message-block">
                         {
-                            isMyMessage ? <MyMessage /> : <TheirMessage />
+                            isMyMessage ? <MyMessage message={message} /> : <TheirMessage message={message} lastMessage={messages[lastMessageKey]} />
                         }
                     </div>
                     <div className="read-receipts" style={{ marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px' }}>
@@ -37,10 +37,28 @@ const ChatFeed = (props) => {
             )
         })
     }
-    renderMessages()
+
+
+    if (!chat) return 'Loading...'
+
 
     return (
-        <div>hey</div>
+        <div className="chat-feed">
+            <div className="chat-title-container">
+                <div className="chat-title">
+                    {chat.title}
+                </div>
+                <div className="chat-subtitle">
+                    {chat.people.map((person) => ` ${person.person.username}`)}
+                </div>
+            </div>
+            {renderMessages()}
+            <div style={{ height: '100px' }} />
+
+            <div className="message-form-container">
+                <MessageForm {...props} chatId={activeChat} />
+            </div>
+        </div>
     )
 
 }
